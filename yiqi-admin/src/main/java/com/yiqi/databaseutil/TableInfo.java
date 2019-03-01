8ec5cfd5f5df61e3bd9802341020cc1966c80c01
zero <12763@windows10.microdone.cn>
2019-03-01 14:39:46 +0800

    封装

--- yiqi-admin/src/main/java/com/yiqi/databaseutil/TableInfo.java
+++ yiqi-admin/src/main/java/com/yiqi/databaseutil/TableInfo.java
@@ -13,6 +13,55 @@ public class TableInfo {
     private String isNullable;//是否可以为空
     private String columnType;//字段类型
     private String columnKey;//主键
+	public String getSort() {
+		return sort;
+	}
+	public void setSort(String sort) {
+		this.sort = sort;
+	}
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	public String getTableComment() {
+		return tableComment;
+	}
+	public void setTableComment(String tableComment) {
+		this.tableComment = tableComment;
+	}
+	public String getColumnName() {
+		return columnName;
+	}
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+	public String getColumnComment() {
+		return columnComment;
+	}
+	public void setColumnComment(String columnComment) {
+		this.columnComment = columnComment;
+	}
+	public String getIsNullable() {
+		return isNullable;
+	}
+	public void setIsNullable(String isNullable) {
+		this.isNullable = isNullable;
+	}
+	public String getColumnType() {
+		return columnType;
+	}
+	public void setColumnType(String columnType) {
+		this.columnType = columnType; 
+	}
+	public String getColumnKey() {
+		return columnKey;
+	}
+	public void setColumnKey(String columnKey) {
+		this.columnKey = columnKey;
+	}
 
+    
 
 }
